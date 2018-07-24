464d8673874547c466f2cf3e4d6d4419a2bba1ed
kmagor <kemagorar@gmail.com>
2018-07-24 14:29:40 +0200

    SUB: definiton of GPIO_seq inside write fcnt
    compile ok
    vopt ok
    sim ok
    UVM: 3 error

--- uvm_tb.sv
+++ uvm_tb.sv
@@ -347,8 +347,7 @@
 
 			function void write(input bus_seq_item t);
 				//bus_seq_item expected_txn;
-				GPIO_seq_item expected_txn;
-				expected_txn = GPIO_seq_item::type_id::create ("expected_txn", this);
+							expected_txn = GPIO_seq_item::type_id::create ("expected_txn", this);
 				//if($cast(expected_txn, t.clone())) `uvm_fatal("COW fatal", "Can't copy sequence item in predictor") //COPY ON WRITE
 			  	if(t.read_not_write == 0) begin // Write
 				  	if(t.addr inside{[32'h0100_0000:32'h0100_001C]}) begin
